ed6404c5f8fff7e62d2d70a2de075b84af12d797
oussamaChteoui <oussama.chteoui@mitsu-digital.com>
2022-08-12 16:32:54 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ExpenseList from './components/ExpenseList';
+import ExpenseForm from './components/ExpenseForm';
+import Alert from './components/Alert';
+import uuid from 'react-uuid';
+
+
+const initialExpenses = [
+  {id:uuid() , charge:"rent", amount:1600},
+  {id:uuid() , charge:"car payment", amount:1800},
+  {id:uuid() , charge:"rcredit card bill", amount:2600}
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  //************** State values ******** */
+  // all expenses , add expense
+  const [expenses, setExpenses] = useState(initialExpenses)
+  //single expense
+  const [charge, setCharge] = useState('')
+  //single amount
+  const [amount, setAmount] = useState('')
 
+ //************** Functions ******** */
+  const handleCharge = e =>{
+    console.log('charge',e.target.value);
+    setCharge(e.target.value)
+  }
+  const handleAmount = e =>{
+    console.log('amount',e.target.value);
+    setAmount(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  // console.log(charge, amount);
+    if(charge != "" && amount >0){
+
+      const singleExpence = {id : uuid(), charge:charge, amount:amount}
+      setExpenses([...expenses, singleExpence])
+      setCharge('');
+      setAmount('');
+
+    }else{
+      //handle Alert called 
+    }
+  }
+  return (
+    <>
+      <Alert/>
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm charge={charge} amount={amount}
+        handleAmount= {handleAmount} handleCharge={handleCharge}
+        handleSubmit={handleSubmit}
+         />
+        <ExpenseList  expenses={expenses}/> 
+      </main>
+      <h1>
+        total spending :{""}
+          <span className="total">
+            ${
+              expenses.reduce((acc, curr) => {
+                return (acc += parseInt( curr.amount));
+              },0)
+              }
+          </span>
+      </h1>
+    </> 
+  ) 
+}
 export default App;
